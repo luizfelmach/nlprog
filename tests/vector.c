b1a055ae648c53008ca0af88b0810ba777a26380
Luiz Felipe Machado <luizfelmach@gmail.com>
2022-12-22 17:17:05 -0300

    Add example lib vector

--- tests/vector.c
+++ tests/vector.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <vector.h>
 
+void vec_show(void *data, void *ctx);
+
 int main() {
+    Vector vec = vector_new();
+
+    int i;
+    for (i = 0; i < 200; i++) {
+        int *value = (int *)malloc(sizeof(int));
+        *value = i;
+        vector_push(vec, value);
+    }
+
+    vector_foreach(vec, vec_show, NULL);
+
+    vector_destroy(vec, free);
     return 0;
+}
+
+void vec_show(void *data, void *ctx) {
+    int *value = (int *)data;
+    printf("%d\n", *value);
 }
