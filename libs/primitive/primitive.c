a86b8c17ac941e657c492e6a2017d6acb03cce04
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-12-22 20:34:36 -0300

    Add lib primitive

---
+++ libs/primitive/primitive.c
@@ -0,0 +1,34 @@
+#include <primitive.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *new_string(char *s) {
+    return strdup(s);
+}
+
+int *new_int(int a) {
+    int *b = (int *)malloc(sizeof(int));
+    *b = a;
+    return b;
+}
+
+double *new_double(double a) {
+    double *b = (double *)malloc(sizeof(double));
+    *b = a;
+    return b;
+}
+
+float *new_float(float a) {
+    float *b = (float *)malloc(sizeof(float));
+    *b = a;
+    return b;
+}
+
+char *new_char(char *c) {
+    char *b = (char *)malloc(sizeof(char));
+    *b = *c;
+    return b;
+}
+
+void nothing(void *a) {
+}
---
+++ libs/primitive/primitive.h
@@ -0,0 +1,11 @@
+#ifndef PRIMITIVE_H
+#define PRIMITIVE_H
+
+char *new_string(char *s);
+int *new_int(int a);
+double *new_double(double a);
+float *new_float(float a);
+char *new_char(char *c);
+void nothing(void *a);
+
+#endif  // PRIMITIVE_H
