9d12e81b11ef5388529c44354e38715caf93b875
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-12-26 08:54:04 -0300

    Update indexer

--- src/indexer.c
+++ src/indexer.c
@@ -1,4 +1,3 @@
-#include <libgen.h>
 #include <map.h>
 #include <primitive.h>
 #include <stdio.h>
@@ -6,80 +5,70 @@
 #include <string.h>
 #include <vector.h>
 
-char path[128];
-
-char *words[10] = {
-    "c++", "java", "js",      "python",   "c#",
-    "c",   "rust", "haskell", "assembly", "rust",
-};
+char *doc0[] = {"gol", "gol", "time", "gol"};
+char *doc1[] = {"carro", "gol", "roubado"};
 
 typedef struct {
     int doc, freq;
     double tf_idf;
-} WIT;
+} Inverted_Index_Item;
 
-WIT *wit_new(int doc, int freq, double tf_idf);
-
+Inverted_Index_Item *inverted_index_item_new(int doc, int freq, double tf_idf);
+void inverted_index_add(Map map, char *word, int doc);
 void inverted_index_show(void *data, void *ctx);
-void inverted_index_value_free(void *data) {
-    Vector vec = (Vector)data;
-    vector_destroy(vec, free);
-}
+void inverted_index_destroy(void *data);
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("error: missing parameters.");
-        exit(1);
-    }
-    strcpy(path, argv[1]);
-    dirname(path);
-    printf("current folder context: %s\n", path);
-    FILE *train = fopen(argv[1], "r");
-    if (!train) {
-        printf("error: can not open file.");
-        exit(1);
-    }
-    fclose(train);
-
     Map inverted_index = map_new();
-
-    for (int i = 0; i < 10; i++) {
-        Pair p = map_get(inverted_index, words[i]);
-
-        if (!p) {
-            Vector vec = vector_new();
-            WIT *new_wit = wit_new(1, 1, 0.4);
-            vector_push(vec, new_wit);
-            map_insert(inverted_index, new_string(words[i]), vec);
-        } else {
-            Vector vec = (Vector)pair_second(p);
-            WIT *new_wit = wit_new(1, 1, 0.4);
-            vector_push(vec, new_wit);
-        }
+    int i;
+    for (i = 0; i < 4; i++) {
+        inverted_index_add(inverted_index, doc0[i], 0);
     }
-
+    for (i = 0; i < 3; i++) {
+        inverted_index_add(inverted_index, doc1[i], 1);
+    }
     map_foreach(inverted_index, inverted_index_show, NULL);
-
-    map_destroy(inverted_index, free, inverted_index_value_free);
-
+    map_destroy(inverted_index, free, inverted_index_destroy);
     return 0;
 }
 
-void vec_show(void *data, void *ctx) {
-    WIT *wit = (WIT *)data;
-    printf("%d %d %f\n", wit->doc, wit->freq, wit->tf_idf);
+Inverted_Index_Item *inverted_index_item_new(int doc, int freq, double tf_idf) {
+    Inverted_Index_Item *iii = calloc(1, sizeof(Inverted_Index_Item));
+    iii->doc = doc;
+    iii->freq = freq;
+    iii->tf_idf = tf_idf;
+    return iii;
+}
+
+void inverted_index_add(Map map, char *word, int doc) {
+    Pair p = map_get(map, word);
+    if (!p) {
+        map_insert(map, new_string(word), vector_new());
+        p = map_get(map, word);
+    }
+    Vector value = pair_second(p);
+    Inverted_Index_Item *iii = vector_at(value, doc);
+    if (!iii) {
+        vector_push(value, inverted_index_item_new(doc, 1, 0));
+    } else {
+        iii->freq += 1;
+    }
 }
 
 void inverted_index_show(void *data, void *ctx) {
     Pair p = (Pair)data;
-    Vector vec = pair_second(p);
-    vector_foreach(vec, vec_show, NULL);
+    char *key = pair_first(p);
+    Vector value = pair_second(p);
+    void fn(void *data, void *ctx) {
+        Inverted_Index_Item *iii = data;
+        printf("%d %d %.2lf     ", iii->doc, iii->freq, iii->tf_idf);
+    }
+    printf("%s  ", key);
+    vector_foreach(value, fn, NULL);
+    printf("\n");
 }
 
-WIT *wit_new(int doc, int freq, double tf_idf) {
-    WIT *wit = (WIT *)calloc(1, sizeof(WIT));
-    wit->doc = doc;
-    wit->freq = freq;
-    wit->tf_idf = tf_idf;
-    return wit;
+void inverted_index_destroy(void *data) {
+    Vector value = data;
+    vector_destroy(data, free);
 }
