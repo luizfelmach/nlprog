b74a1bd7a67d0b0a85f0490fba2ec81768f87e4e
jcquadros <julliepaper@gmail.com>
2022-12-29 11:29:27 -0300

    begin

--- src/nlprog.c
+++ src/nlprog.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    if(argc < 3 ){
+        printf("error: missing parameters.\n");
+        exit(1);
+    }
+
+    char file_indexes_name[2048];
+    
+    sprintf(file_indexes_name, "%s/%s", argv[1], argv[2]);
+
+    FILE *file_indexes = fopen(file_indexes_name, "r");
+
+    printf("info: current folder is '%s'.\n", argv[1]);
+    printf("info: path to file input is '%s'.\n", file_indexes_name);
+
+    if (!file_indexes) {
+        printf("error: can not open file '%s'.\n", file_indexes_name);
+        exit(1);
+    }
+    fclose(file_indexes);
+
+    
     return 0;
 }
