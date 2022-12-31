9a8a6b3201bcebffacecb401b4e079d8e27f2eed
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-12-31 10:54:40 -0300

    Rename method in inverted_index

--- libs/inverted_index/inverted_index.c
+++ libs/inverted_index/inverted_index.c
@@ -88,7 +88,7 @@ void inverted_index_show(Inverted_Index ii) {
     }
 }
 
-void inverted_index_save(Inverted_Index ii, FILE *file) {
+void inverted_index_write(Inverted_Index ii, FILE *file) {
 }
 
 void inverted_index_destroy(Inverted_Index ii) {
--- libs/inverted_index/inverted_index.h
+++ libs/inverted_index/inverted_index.h
@@ -9,7 +9,7 @@ Inverted_Index inverted_index_new();
 Inverted_Index inverted_index_load(FILE *file);
 void inverted_index_add(Inverted_Index ii, char *word, int doc);
 void inverted_index_show(Inverted_Index ii);
-void inverted_index_save(Inverted_Index ii, FILE *file);
+void inverted_index_write(Inverted_Index ii, FILE *file);
 void inverted_index_destroy(Inverted_Index ii);
 
 #endif  // INVERTED_INDEX_H
