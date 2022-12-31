d207c080157e6b3491c4ae43d4195d7441e8cb14
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-12-31 12:26:25 -0300

    Add call method to algorithm

--- libs/algo/algo.h
+++ libs/algo/algo.h
@@ -9,6 +9,8 @@ void vec_show_double(void *data, void *ctx);
 void vec_show_float(void *data, void *ctx);
 void vec_show_char(void *data, void *ctx);
 
-// Map
+// Callback Creator
+
+#define call(return_type, body) ({ return_type _fn body _fn; })
 
 #endif  // ALGO_H
