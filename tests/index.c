ff9ad5f576641f53740f716358cb66dd9c17e3b6
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-01-11 11:52:27 -0300

    Fix variable name

--- tests/index.c
+++ tests/index.c
@@ -2,34 +2,34 @@
 #include <stdio.h>
 
 int main() {
-    Index inverted_index = index_new();
-    Index forward_index = index_new();
+    Index inverted = index_new();
+    Index forward = index_new();
 
-    index_add(inverted_index, "gol", "0");
-    index_add(inverted_index, "gol", "0");
-    index_add(inverted_index, "time", "0");
-    index_add(inverted_index, "gol", "0");
+    index_add(inverted, "gol", "0");
+    index_add(inverted, "gol", "0");
+    index_add(inverted, "time", "0");
+    index_add(inverted, "gol", "0");
 
-    index_add(inverted_index, "carro", "1");
-    index_add(inverted_index, "gol", "1");
-    index_add(inverted_index, "roubado", "1");
+    index_add(inverted, "carro", "1");
+    index_add(inverted, "gol", "1");
+    index_add(inverted, "roubado", "1");
 
-    index_add(forward_index, "123.txt-esp", "1");
-    index_add(forward_index, "123.txt-esp", "1");
-    index_add(forward_index, "123.txt-esp", "1");
-    index_add(forward_index, "123.txt-esp", "3");
+    index_add(forward, "123.txt-esp", "1");
+    index_add(forward, "123.txt-esp", "1");
+    index_add(forward, "123.txt-esp", "1");
+    index_add(forward, "123.txt-esp", "3");
 
-    index_add(forward_index, "456.txt-pol", "0");
-    index_add(forward_index, "456.txt-pol", "1");
-    index_add(forward_index, "456.txt-pol", "2");
+    index_add(forward, "456.txt-pol", "0");
+    index_add(forward, "456.txt-pol", "1");
+    index_add(forward, "456.txt-pol", "2");
 
     printf("~~~~ Inverted Index ~~~~\n\n");
-    index_show(inverted_index);
+    index_show(inverted);
 
     printf("~~~~ Forward Index ~~~~\n\n");
-    index_show(forward_index);
+    index_show(forward);
 
-    index_destroy(inverted_index);
-    index_destroy(forward_index);
+    index_destroy(inverted);
+    index_destroy(forward);
     return 0;
 }
