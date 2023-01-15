034fcdade895af98cf4546e0bfb3108b74289900
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-01-15 10:39:29 -0300

    Update indexer

--- src/indexer.c
+++ src/indexer.c
@@ -8,7 +8,11 @@
 
 int total_docs;
 
+void setup(int argc, char *argv[], Vector *path_docs, Vector *class_docs);
+void show(Index inverted, Index forward);
+void write(int argc, char *argv[], Index inverted, Index forward);
 int ensure_exists_paths(Vector path_docs);
+
 void get_inverted(Index inverted, Vector path_docs);
 void get_forward(Index forward, Index inverted, Vector path_docs,
                  Vector class_docs);
@@ -17,31 +21,44 @@ double calculate_tfidf(int freq_p_in_d, int n_docs_p_appeared);
 int inverted_sort(const void *d1, const void *d2);
 
 int main(int argc, char *argv[]) {
+    Vector path_docs, class_docs;
+    setup(argc, argv, &path_docs, &class_docs);
+
+    Index inverted = index_new();
+    Index forward = index_new();
+
+    get_inverted(inverted, path_docs);
+    index_sort(inverted, inverted_sort);
+    get_forward(forward, inverted, path_docs, class_docs);
+    generate_tfidf(inverted, forward);
+    show(inverted, forward);
+
+    vector_destroy(path_docs, free);
+    vector_destroy(class_docs, free);
+
+    write(argc, argv, inverted, forward);
+
+    index_destroy(inverted);
+    index_destroy(forward);
+    return EXIT_SUCCESS;
+}
+
+void setup(int argc, char *argv[], Vector *path_docs, Vector *class_docs) {
     if (argc < 3) {
-        printf("error: missing parameters.\n");
+        printf("usage: ./indexer <path> <output>.\n");
         exit(1);
     }
-
     char filename_input[2048];
-    char filename_output[2048];
-
     sprintf(filename_input, "%s/train.txt", argv[1]);
-    sprintf(filename_output, "%s/%s", argv[1], argv[2]);
-
     printf("info: current folder is '%s'.\n", argv[1]);
     printf("info: path to file input is '%s'.\n", filename_input);
-    printf("info: path to file output is '%s'.\n", filename_output);
-
     FILE *file_input = fopen(filename_input, "r");
-
     if (!file_input) {
         printf("error: can not open file '%s'.\n", filename_input);
         exit(1);
     }
-
-    Vector path_docs = vector_new();
-    Vector class_docs = vector_new();
-
+    *path_docs = vector_new();
+    *class_docs = vector_new();
     while (1) {
         char temp1[1024], temp2[1024];
         char *path = malloc(sizeof(char) * 1024);
@@ -53,31 +70,20 @@ int main(int argc, char *argv[]) {
         }
         sprintf(path, "%s/%s", argv[1], temp1);
         sprintf(class, "%s", temp2);
-        vector_push(path_docs, path);
-        vector_push(class_docs, class);
+        vector_push(*path_docs, path);
+        vector_push(*class_docs, class);
     }
     fclose(file_input);
-
-    if (!ensure_exists_paths(path_docs)) {
+    if (!ensure_exists_paths(*path_docs)) {
         printf("error: missing files in '/train'.\n");
-        vector_destroy(path_docs, free);
-        vector_destroy(class_docs, free);
+        vector_destroy(*path_docs, free);
+        vector_destroy(*class_docs, free);
         exit(1);
     }
+    total_docs = vector_size(*path_docs);
+}
 
-    total_docs = vector_size(path_docs);
-
-    Index inverted = index_new();
-    Index forward = index_new();
-
-    get_inverted(inverted, path_docs);
-    index_sort(inverted, inverted_sort);
-    get_forward(forward, inverted, path_docs, class_docs);
-    generate_tfidf(inverted, forward);
-
-    vector_destroy(path_docs, free);
-    vector_destroy(class_docs, free);
-
+void show(Index inverted, Index forward) {
     // run to show: INDEX_SHOW= ./indexer ...
     if (getenv("INDEX_SHOW")) {
         printf("\n-------------- inverted --------------\n\n");
@@ -85,27 +91,24 @@ int main(int argc, char *argv[]) {
         printf("\n-------------- forward --------------\n\n");
         index_show(forward);
     }
-
     printf("info: %d words.\n", index_size(inverted));
     printf("info: %d docs.\n", index_size(forward));
+}
 
+void write(int argc, char *argv[], Index inverted, Index forward) {
+    char filename_output[2048];
+    sprintf(filename_output, "%s/%s", argv[1], argv[2]);
     FILE *file_output = fopen(filename_output, "wb");
-
     if (!file_output) {
         printf("error: can not write output in '%s'.", filename_output);
         index_destroy(inverted);
         index_destroy(forward);
         exit(1);
     }
-
+    printf("info: writing in '%s'.\n", filename_output);
     index_write(inverted, file_output);
     index_write(forward, file_output);
-
-    printf("info: ending and closing program.\n");
     fclose(file_output);
-    index_destroy(inverted);
-    index_destroy(forward);
-    return 0;
 }
 
 int ensure_exists_paths(Vector path_docs) {
